6dfb10c305bf3ded065d0ea99660d38a4ce99f9c
Dave Bohrk <dabohrk@twilio.com>
2021-07-18 13:45:35 -0400

    Initial commit

---
+++ internal/handlers/handlers.go
@@ -0,0 +1,62 @@
+package handlers
+
+import (
+	"github.com/CloudyKit/jet/v6"
+	"github.com/gorilla/websocket"
+	"log"
+	"net/http"
+)
+
+var views = jet.NewSet(
+	jet.NewOSFileSystemLoader("./html"),
+	jet.InDevelopmentMode(),
+)
+
+var upgradeConnection = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin:     func(r *http.Request) bool { return true },
+}
+
+func Home(w http.ResponseWriter, r *http.Request) {
+	err := renderPage(w, "home.jet", nil)
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+type WsJsonResponse struct {
+	Action  string `json:"action"`
+	Message string `json:"message"`
+	MessageType string `json:"message_type"`
+}
+// WsEndpoint upgrades commection to websoc
+func WsEndpoint(w http.ResponseWriter, r *http.Request) {
+	ws, err := upgradeConnection.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+	}
+	log.Println("Client connected to endpoint")
+	var response WsJsonResponse
+	response.Message = `<em><small>Conected to Server</small></em>`
+
+	err = ws.WriteJSON(response)
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func renderPage(w http.ResponseWriter, tmpl string, data jet.VarMap) error {
+	view, err := views.GetTemplate(tmpl)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	err = view.Execute(w, data, nil)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+	return nil
+}
